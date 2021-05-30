b643d7ee185a99eb1465169edc85ba35fd91ebc7
Ejike Chinecherem Victory <56884436+victoryejike@users.noreply.github.com>
2021-05-31 00:05:10 +0100

    displaying singlebootcamps

--- src/Pages/SingleBootcamp/SingleBootcamp.js
+++ src/Pages/SingleBootcamp/SingleBootcamp.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./SingleBootcamp.scss";
 
 const SingleBootcamp = ({ data, match }) => {
+  const [bootcamp, setBootcamp] = useState([]);
+
+  const getSingleBootcamp = () => {
+    const getBootcamp = data.filter(
+      (singlebootcamp) => singlebootcamp.bootcamp_id === match.params.id
+    );
+    setBootcamp(getBootcamp);
+  };
+
+  useEffect(() => {
+    getSingleBootcamp();
+  });
+
   return (
     <>
-      <p>A Single Bootcamp</p>
+      {bootcamp.map((onebootcamp) => (
+        <p>{onebootcamp.bootcamp_name}</p>
+      ))}
     </>
   );
 };
