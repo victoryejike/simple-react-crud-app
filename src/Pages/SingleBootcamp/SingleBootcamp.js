0c6fe07b4d16616f843e3973293a16bbee2b9a29
Ejike Victory <victoryejike@gmail.com>
2021-05-31 19:04:51 +0100

    fixed the issue with the params turns out it was because of strict equality

--- src/Pages/SingleBootcamp/SingleBootcamp.js
+++ src/Pages/SingleBootcamp/SingleBootcamp.js
@@ -6,7 +6,7 @@ const SingleBootcamp = ({ data, match }) => {
 
   const getSingleBootcamp = () => {
     const getBootcamp = data.filter(
-      (singlebootcamp) => singlebootcamp.bootcamp_id === match.params.id
+      (singlebootcamp) => singlebootcamp.bootcamp_id == match.params.id
     );
     setBootcamp(getBootcamp);
   };
